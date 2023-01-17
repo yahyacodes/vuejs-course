17254396767e128fec7785f0089c3e677587b6da
yahyacodes <yahya.codin@gmail.com>
2023-01-17 20:37:47 +0300

    m

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <h1>{{ title }}</h1>
   <p>Welcome....</p>
-  <div v-if="showModal">
+  <Teleport to=".modals" v-if="showModal">
     <ModalVue theme="sale" @close="toggleModal">
       <template v-slot:links>
         <a href="#">Sign up now</a>
@@ -10,36 +10,55 @@
       <h1>Ninja Giveaway</h1>
       <p>Grab your ninja swag for half price</p>
     </ModalVue>
-  </div>
+  </Teleport>
+
+  <Teleport to=".modals" v-if="showModalTwo">
+    <ModalVue @close="toggleModalTwo">
+      <h1>This is the second modal</h1>
+      <p>This is modal two practice in vuejs</p>
+    </ModalVue>
+  </Teleport>
+
   <button @click="toggleModal">open modal</button>
+  <button @click="toggleModalTwo">open modal two</button>
 </template>
 
 <script>
+// Challange One
+// - create an extra button to open different modal
+// - use the same modal componet but pass different template (slot)
+// - use a different method (eg toggleModalTwo) and data (eg showModalTwo)
+
+import { Teleport } from "vue";
 import ModalVue from "./components/ModalVue.vue";
 
 export default {
   name: "App",
   components: {
     ModalVue,
+    Teleport,
   },
   data() {
     return {
       title: "My first vue App",
-      header: "Sign up for the Giveaway!!",
-      text: "Grab your ninja swag for half price",
       showModal: false,
+      showModalTwo: false,
     };
   },
   methods: {
     toggleModal() {
       this.showModal = !this.showModal;
     },
+    toggleModalTwo() {
+      this.showModalTwo = !this.showModalTwo;
+    },
   },
 };
 </script>
 
 <style>
-#app {
+#app,
+.modals {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
